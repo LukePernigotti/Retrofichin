f4cbae7b66d09f4c344f5dbd3519c13e0e8a3943
LukePernigotti <luke.pernigotti.90@gmail.com>
2019-05-21 10:41:10 -0300

    Updates

---
+++ precache-manifest.9aba6879a6c43478d819ecfdb3c769ab.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a598145d68772014f8a531fb2e56104",
+    "url": "/retrofichin/index.html"
+  },
+  {
+    "revision": "4f59787b18b656aac5d0",
+    "url": "/retrofichin/static/js/2.f1fe207b.chunk.js"
+  },
+  {
+    "revision": "3a6aeae6928be5c24e4c",
+    "url": "/retrofichin/static/js/main.9d592300.chunk.js"
+  },
+  {
+    "revision": "820e90167c43a20a36d2",
+    "url": "/retrofichin/static/js/runtime~main.f2da76bb.js"
+  }
+]);
